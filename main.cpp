e912977cbd1e11581c7b58c9f4da6dd6f065fa4b
MikeBas1029 <98618592+MikeBas1029@users.noreply.github.com>
2024-02-19 02:35:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,43 @@
 #include <sys/wait.h>
 
 //compile to rest g++ -o test main.cpp
-
-bool varToPipe(){   //to determine if input var will be inside pipe, if false then it's another pipe going into another
-    return false;   //write next
-}
-
-bool arthVarToPipe(){   //to determine if arithmetic is being used with the input var
-    return false;       //write next
-}
-
-bool arthPipeToPipe(){  //to determine if arithmetic is being used with the pipes
-    return false;       //write next
-}
-
 using namespace std;
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//functions
+
+bool checkOper(const vector<string>& input, int pos){
+    char operations[4] = {'-', '+', '*', '/'};
+    for(int i = 0; i < input.size(); i++){
+        if (input.at(i).find(operations[0])) {
+            return true;
+        } else if (input.at(i).find(operations[1])) {
+            return true;
+        } else if (input.at(i).find(operations[2])) {
+            return true;
+        } else if (input.at(i).find(operations[3])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+int whichOut(const vector<string>& updated, int pos){
+    return stoi(updated.at(pos).substr(updated.at(pos).find('p')+1));
+}
+
+int doOper(const vector<string>& input, int pos){
+
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 int main(int argc, char** argv) {
     vector<int> inputValList;   //input val in dynamic data struct do allow versatile amount of input
     vector<string> input;
     vector<string> updating;
-    //pid_t pId;       //internal var
-    //int pipeFD[2];
-    char operations[4] = {'-', '+', '*', '/'};
+    int pipes[4][2];            //p0, p1, p2, p3 w/ read or write
 
     if(argc != 3){  //error if input not provided
         cout<< "No input argument is found.\n";
@@ -75,20 +91,59 @@ int main(int argc, char** argv) {
     }
     fileOper.close();   //Closing rather than continuing the process for less nesting
 
-    for(int x = 0; x < input.size(); x++){
-        if(varToPipe()){
-            if(arthVarToPipe()){
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //pipe action
 
-            }else{
-
-            }
-        }else{
-            if(arthPipeToPipe()){
-
-            }else{
-
-            }
+    for(int i = 0; i < 4; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("Pipe");
+            return 1;
         }
+    }
+
+    for(int i =0; i < 4; i++){
+        pid_t pID = fork();
+        if(pID == -1){
+            perror("Fork");
+        }
+        if(pID == 0) {                       //child process
+            close(pipes[i][1]);
+            if (i > 0) {
+                close(pipes[i - 1][0]);
+            }
+
+            int result;
+            read(pipes[i][0], &result, sizeof(result));
+            cout<< "p" << i << " = "<< result<< endl;
+            //write for if that pipe is using operations
+
+            close(pipes[i][0]);         // Close read end
+            return 0;
+        }
+        close(pipes[i][0]);             //parent process
+    }
+
+
+    //input values in each pipe
+    int result;
+    for(int i = 0; i < inputValList.size(); i++){
+        if(!checkOper(input, i) && i > 0){
+            result = doOper(input, i);
+            if(i == whichOut(updating, i)){
+
+            }else if(i < whichOut(updating, i)){
+
+            }
+        }else if(isPipe(input, i)){
+
+        }else{
+            write(pipes[i][1], &inputValList.at(i), sizeof(inputValList.at(i)));
+        }
+    }
+
+    //waiting for all child processes
+    for(int i =0; i< 4; i++){
+        wait(nullptr);
     }
 
     return 0;
